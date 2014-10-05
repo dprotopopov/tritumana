03fc887fa8746e8fe7e30e57eba39ce77afa1c93
dprotopopov <protopopov@narod.ru>
2014-10-05 20:42:59 +0400

    install

---
+++ install.php
@@ -0,0 +1,15 @@
+﻿<?php
+//////////////////////////////////////////////////////////////////////////////
+// Разрабочик dmitry@protopopov.ru
+require_once( dirname(__FILE__) . '/application.php' );
+require_once( dirname(__FILE__) . '/factory.php' );
+
+	// http://stackoverflow.com/questions/486181/php-suppress-output-within-a-function
+	ob_start();
+
+	$app = JFactory::getApplication();
+
+	// http://stackoverflow.com/questions/486181/php-suppress-output-within-a-function
+	ob_end_clean();
+
+	$app->rebuild_database();		
